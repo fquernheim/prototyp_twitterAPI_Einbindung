f15a6632b6cd66f6d1f907cb7f147e5988c23242
FQu99 <61802565+FQu99@users.noreply.github.com>
2020-05-26 19:09:49 +0200

    twitteroauth eingebunden und Controller erstellt

---
+++ TwitterAPI/app/Http/Controllers/authentificationController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+require "twitteroauth/autoload.php";
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+
+class authentificationController extends Controller
+{
+    //
+}
